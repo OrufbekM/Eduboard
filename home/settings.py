cd58b38079591e6c085dd64187b9adae7e89943b
OrufbekM <orufbekmaxmudjonov77@gmail.com>
2025-12-15 16:05:22 +0500

    Enhance settings for static file handling by adding WhiteNoise middleware and updating STATIC_URL format.

--- home/settings.py
+++ home/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,7 +123,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/' 
 MEDIA_ROOT = BASE_DIR / 'media' 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
